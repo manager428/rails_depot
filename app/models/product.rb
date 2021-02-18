43987e249af61032adb91364b8e410340f59acc8
manager428 <sweetbigdream428@gmail.com>
2021-02-18 22:34:51 +0900

    creating the catalog listing, adding a page layout

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,17 @@
 class Product < ApplicationRecord
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+
+  # ensure that there are no line items referencing this product
+  def ensure_not_referenced_by_any_line_item
+    return if line_items.empty?
+
+    errors.add(:base, 'Line Items present')
+    thorw :abort
+  end
+
   validates :title, uniqueness: true
   validates :title, :description, :image_url, presence: true
   validates :image_url, allow_blank: true, format: {
