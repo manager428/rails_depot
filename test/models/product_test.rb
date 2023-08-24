73b2d51da4e39d4a085897a3a1018d2465af7e71
manager428 <sweetbigdream428@gmail.com>
2023-08-24 12:01:13 +0900

    completed first unit test

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,8 +1,8 @@
-require "test_helper"
+require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
   fixtures :products # load the fixtures from test/fixtures/products.yml
-  test "product attributes must not be empty" do
+  test 'product attributes must not be empty' do
     product = Product.new
     assert product.invalid?
     assert product.errors[:title].any? # check if the product object has any errors on the title field
@@ -11,35 +11,35 @@ class ProductTest < ActiveSupport::TestCase
     assert product.errors[:image_url].any? # check if the product object has any errors on the image_url field
   end
 
-  test "product price must be positive" do
+  test 'product price must be positive' do
     product = Product.new(
-      title:        "My Book Title", 
-      description:  "yyy", 
-      image_url:    "zzz.jpg"
+      title: 'My Book Title',
+      description: 'yyy',
+      image_url: 'zzz.jpg'
     )
     product.price = -1
     assert product.invalid?
-    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+    assert_equal ['must be greater than or equal to 0.01'], product.errors[:price]
 
     product.price = 0 # set the price to 0
-    assert product.invalid? 
-    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+    assert product.invalid?
+    assert_equal ['must be greater than or equal to 0.01'], product.errors[:price]
 
     product.price = 1
-    assert product.valid? 
+    assert product.valid?
   end
 
   def new_product(image_url)
     Product.new(
-      title:        "My Book Title",
-      description:  "yyy",
-      price:        1,
-      image_url:    image_url
-    )    
+      title: 'My Book Title',
+      description: 'yyy',
+      price: 1,
+      image_url:
+    )
   end
-  test "image url" do
-    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif } # %w{...} is a shortcut for an array of strings
-    bad = %w{ fred.doc fred.gif/more fred.gif.more } # %w{...} is a shortcut for an array of strings
+  test 'image url' do
+    ok = %w[fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif] # %w{...} is a shortcut for an array of strings
+    bad = %w[fred.doc fred.gif/more fred.gif.more] # %w{...} is a shortcut for an array of strings
     ok.each do |image_url|
       assert new_product(image_url).valid?, "#{image_url} should be valid"
     end
