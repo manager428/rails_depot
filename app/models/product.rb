02970ca8e871e4aa83e72d242e188eda55a3a075
manager428 <sweetbigdream428@gmail.com>
2023-08-24 21:37:04 +0900

    creating the catalog listing, adding a page layout

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,29 +1,9 @@
 class Product < ApplicationRecord
-
   validates :title, uniqueness: true
-
-
-
   validates :title, :description, :image_url, presence: true
-
-
-
   validates :image_url, allow_blank: true, format: {
-
-
-
     with: /\.(gif|jpg|png)\Z/i,
-
-
-
     message: 'must be a URL for GIF, JPG or PNG image.'
-
-
-
   }
-
-
-
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
-
 end
