4bad3230557bbf88d68352154ff463737187e328
manager428 <sweetbigdream428@gmail.com>
2023-08-26 08:55:35 +0900

    partial, turbo_stream

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -23,10 +23,11 @@ class LineItemsController < ApplicationController
   # POST /line_items or /line_items.json
   def create
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
+    @line_item = @cart.add_product(product)
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to cart_url(@line_item.cart), notice: "Line item was successfully created." }
+        format.turbo_stream
+        format.html { redirect_to store_index_url }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -40,11 +41,9 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.update(line_item_params)
         format.html { redirect_to line_item_url(@line_item), notice: "Line item was successfully updated." }
-
         format.json { render :show, status: :ok, location: @line_item }
       else
         format.html { render :edit, status: :unprocessable_entity }
-
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
       end
     end
@@ -56,7 +55,6 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to line_items_url, notice: "Line item was successfully destroyed." }
-
       format.json { head :no_content }
     end
   end
